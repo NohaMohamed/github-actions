5d7a7558d053f82fe9e2f5209a44565640a1ce6e
noha-mohamed-ali <noha@audiomob.com>
2024-07-22 15:01:13 +0400

    Initial Commit

---
+++ UnitTestGithub/UnitTestGithub.h
@@ -0,0 +1,18 @@
+//
+//  UnitTestGithub.h
+//  UnitTestGithub
+//
+//  Created by Noha on 7/22/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnitTestGithub.
+FOUNDATION_EXPORT double UnitTestGithubVersionNumber;
+
+//! Project version string for UnitTestGithub.
+FOUNDATION_EXPORT const unsigned char UnitTestGithubVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnitTestGithub/PublicHeader.h>
+
+
