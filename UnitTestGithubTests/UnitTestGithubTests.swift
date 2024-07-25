3eb1c0f85ab5a60e234f9dc5c41cd8ff39c9c5b2
noha-mohamed-ali <noha@audiomob.com>
2024-07-25 10:36:10 +0400

    Update tests

--- UnitTestGithubTests/UnitTestGithubTests.swift
+++ UnitTestGithubTests/UnitTestGithubTests.swift
@@ -24,14 +24,6 @@ final class UnitTestGithubTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        XCTAssertEqual(90, 20)
+        XCTAssertEqual(90, 90)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
